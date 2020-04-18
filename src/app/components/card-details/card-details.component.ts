6b6d8c27d560b59e898e3a5c6e4da0872888306e
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-18 16:46:57 -0500

    card details

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/models/Card.model';
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
+import { CardService } from 'src/app/services/card.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card-details',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-details.component.scss']
 })
 export class CardDetailsComponent implements OnInit {
+  public card: Card = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  public get ready(): boolean {
+    return BooleanHelper.notNull(this.card);
   }
+
+  constructor(
+    private cardService: CardService,
+    private route: ActivatedRoute,
+  ) { }
+
+  public ngOnInit() {
+    this.loadApp();
+  }
+
+  private loadApp(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.cardService.getSingle(id)
+      .subscribe((res) => this.card = res,
+        (error) => {
+          console.log("get card failed");
+        });
+  }
+
 
 }
